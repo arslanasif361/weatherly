69286bea656ae7a7c937b74188200b8eb935ca31
Salman Shaikh <sshaik1010@gmail.com>
2021-03-27 15:00:52 +0500

    my project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[900],
+        title: Center(child: Text("I am Poor")),
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/poor.jpg'),
+        ),
+      ),
+    ),
+  ));
+}
